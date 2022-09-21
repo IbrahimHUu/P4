50453a349837bb8e81e0be8c1ddaab3d1c888ba3
IbrahimHUu <94849299+IbrahimHUu@users.noreply.github.com>
2022-09-21 15:12:22 +0200

    Add files via upload

---
+++ P_OVchipkaart/src/DAO/OVchipkaartDAO.java
@@ -0,0 +1,16 @@
+package DAO;
+
+import Domeinklasse.OVchipkaart;
+import Domeinklasse.Reiziger;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface OVchipkaartDAO {
+
+    List<OVchipkaart> findByReiziger (Reiziger reiziger);
+    boolean save(OVchipkaart ovchipkaart);
+    boolean update (OVchipkaart ovchipkaart);
+    boolean delete (OVchipkaart ovchipkaart);
+    List<OVchipkaart> findAll();
+}
